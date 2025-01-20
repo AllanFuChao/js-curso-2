3662e67fc5922b912a02ae24bb3e08019b2d0121
afuc <afuc@usma.com.pa>
2025-01-20 14:54:44 -0500

    Resolvi los desafios en una sola
    Desafíos
    Descarga otro proyecto haciendo clic en este enlace y ábrelo en Visual Studio Code.
    Cambia el contenido de la etiqueta h1 con document.querySelector y asigna el siguiente texto: "Hora del Desafío".
    Crea una función que muestre en la consola el mensaje "El botón fue clicado" siempre que se presione el botón "Console".
    Crea una función que se ejecute cuando se haga clic en el botón "prompt", preguntando el nombre de una ciudad de Brasil. Luego, muestra una alerta con el mensaje concatenando la respuesta con el texto: "Estuve en {ciudad} y me acordé de ti".
    Crea una función que muestre una alerta con el mensaje: "Yo amo JS" siempre que se presione el botón "Alerta".
    Al hacer clic en el botón "suma", pide 2 números y muestra el resultado de la suma en una alerta.

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+let header = document.querySelector('h1');
+header.innerHTML = 'Hora del Desafio.';
 
+function botonConsole() {
+    console.log("El boton fue Console fue clicado")
+}
+
+function botonPrompt() {
+    let ciudadDeBrasil = prompt("Escribe el nombre de una ciudad de Brasil:");
+    alert(`Estuve en ${ciudadDeBrasil} y me acorde de ti.`);
+}
+
+function botonAlert() {
+    alert('Yo amo JS');
+}
+
+function botonSuma() {
+    let numeroUno = parseFloat(prompt('Escribe el primer valor a sumar:'));
+    let numeroDos = parseFloat(prompt('Escribe el segundo valor a sumar:'));
+    let sumaDeNumeros = numeroUno + numeroDos;
+    alert(`La suma de tus numeros es: ${sumaDeNumeros}`);
+}
